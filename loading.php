665533a90311337454c4bf648e8a1e43bc5bb59c
dereckyin <dereckyin@yahoo.com.tw>
2021-12-29 15:15:47 +0800

    taiwan read and phili read 02

--- loading.php
+++ loading.php
@@ -104,7 +104,7 @@ $(function(){
              <div class="block">
                 <div class="btnbox">
                     <?php
-                    if($taiwan_read == "1")
+                    if($taiwan_read == "0")
                     {
                     ?>
                      <a class="btn small detail">新增貨櫃記錄<eng>New Container Record</eng></a>
