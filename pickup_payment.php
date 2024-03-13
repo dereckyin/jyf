191d7f3f270bb4771e92aa579cda01eb9cb2e0c1
dereckyin <dereckyin>
2024-03-13 10:38:56 +0800

    Update pickup_payment.php

--- pickup_payment.php
+++ pickup_payment.php
@@ -284,6 +284,16 @@ header( 'location:index.php' );
             align-items: center;
         }
 
+        div.block > .listheader input[type='date'] {
+            order: 1px solid #999;
+            border-radius: 5px;
+            background-color: #fff;
+            padding: 5px;
+            vertical-align: middle;
+            width: 160px;
+            margin-right: 10px;
+        }
+
         #export_modal .tablebox.s02 textarea{
             height: auto;
         }
