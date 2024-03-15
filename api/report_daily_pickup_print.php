037afe4eb4f60ad1f3fc6dc8f5893f2e605e7243
dereckyin <dereckyin>
2024-03-15 09:19:40 +0800

    Update report_daily_pickup_print.php

--- api/report_daily_pickup_print.php
+++ api/report_daily_pickup_print.php
@@ -157,8 +157,11 @@ if($jwt){
                 foreach($result as $rec)
                 {
                     $sheet->setCellValue('A' . $i, $rec["pick_date"]);
+                    $sheet->getColumnDimension('D')->setWidth(12);
                     $sheet->setCellValue('B' . $i, $rec["customer"]);
+                    $sheet->getColumnDimension('D')->setWidth(50);
                     $sheet->setCellValue('C' . $i, $rec["encode"]);
+                    $sheet->getColumnDimension('D')->setWidth(12);
 
                     $sheet->getStyle('A'. $i. ':' . 'C' . $i)->applyFromArray($styleArray);
                     $i++;
